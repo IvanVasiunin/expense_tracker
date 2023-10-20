bdb0d67d27961ac19f65261519f6663accab9ea3
Ivan <ivanvasyunin@gmail.com>
2023-10-20 15:42:20 +0200

    styles added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import ExpenseItem from './ExpenseItem';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <ExpenseItem />
     </div>
   );
 };
